79f0730047e400e19a5f5c68fc1bb8e033a6d839
tasidora08 <tasi.dora08@gmail.com>
2026-04-29 11:48:21 +0200

    Add project files.

---
+++ Sebesseg/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Sebesseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> adatok = new List<string[]>();
+            StreamReader sr = new StreamReader("ut.txt");
+            string[] elsoSor = sr.ReadLine().Split();
+            while (!sr.EndOfStream)
+            {
+                string[] sor = sr.ReadLine().Split();
+                adatok.Add(sor);
+            }
+            Console.WriteLine($"2. feladat\nA települések neve:");
+            foreach (string[] sor in adatok)
+            {
+                string sorMasodik = sor[1];
+                if (sorMasodik.Length >= 4)
+                {
+                    Console.WriteLine(sor[1]);
+                }
+            }
+            Console.WriteLine("\n3. feladat");
+        }
+    }
+}
